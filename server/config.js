9fc6e267581b58b51ec4f4278403240ecbcac21b
서재원 <ssjaewon1008@naver.com>
2026-08-01 18:08:44 +0900

    1808

--- server/config.js
+++ server/config.js
@@ -5,8 +5,10 @@ export const config = {
   host: process.env.HOST || '127.0.0.1',
   frontendOrigin: process.env.FRONTEND_ORIGIN || 'http://127.0.0.1:5173',
   kiwoomHost: process.env.KIWOOM_API_HOST || 'https://api.kiwoom.com',
-  kiwoomAppKey: process.env.KIWOOM_APPKEY || process.env.app_key || '',
-  kiwoomSecretKey: process.env.KIWOOM_SECRETKEY || process.env.secret_key || '',
+  kiwoomAppKey: process.env.KIWOOM_APP_KEY || process.env.kiwoom_app_key || '',
+  kiwoomSecretKey: process.env.KIWOOM_SECRET_KEY || process.env.kiwoom_secret_key || '',
+  tossApiKey: process.env.TOSS_API_KEY || process.env.toss_api_key || '',
+  tossSecretKey: process.env.TOSS_SECRET_KEY || process.env.toss_secret_key || '',
   naverClientId: process.env.NAVER_CLIENT_ID || process.env.client_id || '',
   naverClientSecret: process.env.NAVER_CLIENT_SECRET || process.env.client_secret || '',
 }
@@ -14,6 +16,7 @@ export const config = {
 export function publicConfigurationStatus() {
   return {
     kiwoomConfigured: Boolean(config.kiwoomAppKey && config.kiwoomSecretKey),
+    tossConfigured: Boolean(config.tossApiKey && config.tossSecretKey),
     naverConfigured: Boolean(config.naverClientId && config.naverClientSecret),
   }
 }
